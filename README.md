10ffdba5237c3c2b20b00e9fa53275d4c3cc25c6
Alessio toshi <alegue87@gmail.com>
2024-01-10 13:08:41 -0500

    aggiunto readme

---
+++ README.md
@@ -0,0 +1,16 @@
+
+installazione:
+
+necessario python3 e pip3
+
+sudo apt install python3 python3-pip
+
+installare le seguenti libs:
+
+sudo apt install zlib1g-dev libjpeg-dev libpng-dev
+
+quindi avviare l'installazione dei requirements
+
+(di pillow usa la 10.2.0)
+
+pip install -r requirements.txt --break-system-packages
--- gui.py
+++ gui.py
@@ -159,7 +159,7 @@ class SubHandler(object):
         print("Python: New event", event)
 
 def connect():
-    client = Client("opc.tcp://192.168.1.106:4840")
+    client = Client("opc.tcp://192.168.1.115:4840")
     #client = Client("opc.tcp://localhost:4840")
     # client = Client("opc.tcp://admin@localhost:4840/freeopcua/server/") #connect using a user
     client.connect()
